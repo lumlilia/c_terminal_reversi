7ef0fc3dd2ea9875f03b4c8adbf0709383b54504
lumlilia <136717338+lumlilia@users.noreply.github.com>
2024-04-04 23:19:50 +0900

    ふぁいる分けその弐

--- main.c
+++ main.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <termios.h>
+
+#include "term_change.c"
 #include "reversi_base.h"
 
 
 int main(void){
-  setvbuf(stdout, NULL, _IONBF, 0);
-  struct termios term0, term1;
+  struct termios terms[2];
 
-  tcgetattr(0, &term1);
-  term0 = term1;
-  term1.c_lflag &= ~(ICANON | ECHO);
-  term1.c_cc[VMIN] = 1;
-  term1.c_cc[VTIME] = 0;
-  tcsetattr(0, TCSANOW, &term1);
-
+  GetDefaultTerms(&terms[0]);
+  TermModeChange(&terms[0], 1);
 
   ReversiMain(0, 0);
 
-  tcsetattr(0, TCSANOW, &term0);
+  TermModeChange(&terms[0], 0);
+
   return 0;
 }
-
-
